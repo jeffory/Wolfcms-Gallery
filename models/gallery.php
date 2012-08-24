6be7f7e735ee75d2e0527557ce35cf6a844ecdd1
Keith McGahey <keef05@gmail.com>
2012-08-24 16:56:12 +1000

    Fixed up the SQL and added triggers
    Triggers used for automatically updating creation and modification
    datetimes.

--- models/gallery.php
+++ models/gallery.php
@@ -62,49 +62,48 @@ class Gallery extends Record
 		self::ITEMS_TABLE => array(
 			'id' => array(
 					'int(16)',
+					'NOT NULL',
 					'AUTO_INCREMENT',
 					'PRIMARY KEY'
 				),
 			'name' => array(
-					'varchar(256)',
+					'varchar(255)',
 					'NOT NULL'
 				),
 			'code' => array(
-					'varchar(256)'
+					'varchar(255)'
 				),
 			'description' => array(
 					'text',
 					'NOT NULL'
 				),
 			'image_url' => array(
-					'varchar(256)',
+					'varchar(255)',
 					'NOT NULL'
 				),
-			'date_created' => array(
-					'TIMESTAMP',
-					'DEFAULT CURRENT_TIMESTAMP'
+			'created' => array(
+					'DATETIME',
 				),
-			'date_modified' => array(
-					'TIMESTAMP',
-					'DEFAULT CURRENT_TIMESTAMP',
-					'ON UPDATE CURRENT_TIMESTAMP'
+			'modified' => array(
+					'DATETIME',
 				)
 			),
 		self::CATEGORY_TABLE => array(
 			'id' => array(
 					'int(16)',
+					'NOT NULL',
 					'AUTO_INCREMENT',
 					'PRIMARY KEY'
 				),
 			'category' => array(
-					'varchar(256)',
+					'varchar(255)',
 					'NOT NULL'
 				)
 			)
 		);
 
 	/**
-	 * Create gallery tables
+	 * Create all tables needed for the plugin
 	 *
 	 * @return void
 	 **/
@@ -115,10 +114,11 @@ class Gallery extends Record
 
 		foreach (self::$database_schema as $table_name => $table_details)
 		{
+			$extra_SQL = '';
+
 			if (isset($table_name) && !empty($table_name))
 			{
-				// Add the table prefix, if any, to the table name
-				$table_name = TABLE_PREFIX. $table_name;
+				$table_name = TABLE_PREFIX. $table_name;	// Add the table prefix, if any, to the table name
 
 				$SQL .= "CREATE TABLE IF NOT EXISTS `$table_name` (\n";
 				$table_primary_key = null;
@@ -126,6 +126,17 @@ class Gallery extends Record
 				foreach ($table_details as $column_name => $column_details)
 				{
 					$SQL .= "  `$column_name`";
+
+					// Create any triggers or additional functions based off column names
+					if ($column_name == 'created')
+					{
+						$extra_SQL .= "\nCREATE TRIGGER `{$table_name}_creation` BEFORE INSERT ON `{$table_name}` FOR EACH ROW SET NEW.{$column_name} = NOW();";
+					}
+					elseif ($column_name == 'modified')
+					{
+						$extra_SQL .= "\nCREATE TRIGGER `{$table_name}_modified` BEFORE UPDATE ON `{$table_name}` FOR EACH ROW SET NEW.{$column_name} = NOW();";
+					}
+
 
 					foreach ($column_details as $column_detail)
 					{
@@ -152,9 +163,9 @@ class Gallery extends Record
 
 				if (isset($table_primary_key))
 				{
-					$SQL .= "PRIMARY KEY (`$table_primary_key`)\n";
+					$SQL .= "  PRIMARY KEY (`$table_primary_key`)\n";
 				}
-				$SQL = trim($SQL). ";\n\n";
+				$SQL = trim($SQL). "\n);\n". $extra_SQL. "\n\n";
 			}
 			else
 			{
@@ -163,17 +174,87 @@ class Gallery extends Record
 		} // END foreach table in schema
 
 		echo '<pre>'. $SQL. '</pre>';
+		self::execSQL($SQL);
 	}
 
 	/**
-	 * Delete store detail/option tables
+	 * Deletes all the table associated with the plugin
 	 *
 	 * @return void
 	 **/
 	static public function deleteTables()
 	{
-		// Remove database tables associated with store
-		self::dropTable(TABLE_PREFIX. self::ITEMS_TABLE);
-		self::dropTable(TABLE_PREFIX. self::CATEGORY_TABLE);
+		$SQL = '';
+
+		foreach (self::$database_schema as $table_name => $table_details)
+		{
+			if (isset($table_name) && !empty($table_name))
+			{
+				$table_name = TABLE_PREFIX. $table_name;	// Add the table prefix, if any, to the table name
+
+				$SQL .= "DROP TABLE IF EXISTS `$table_name`;\n\n";
+			}
+		}
+		echo '<pre>'. $SQL. '</pre>';
+		self::execSQL($SQL);
+	}
+
+
+	/**
+	 * Execute a SQL query, return the result object
+	 *
+	 * @param string sql query
+	 * @param string sql arguments (optional)
+	 *
+	 * @return boolean returns true if no errors
+	 **/
+	static private function execSQL($sql_query, $arguments=null, $quotes=true)
+	{
+		$orig_query = $sql_query;
+
+		if ($pdo = Record::getConnection())
+		{
+			if (!empty($arguments)) {
+				for ($i = 0; $i < count($arguments); $i++) {
+					if ($quotes === true)
+					{
+						$arguments[$i] = "'". addslashes(htmlspecialchars($arguments[$i], ENT_QUOTES)). "'";
+					}
+					else
+					{
+						$arguments[$i] = addslashes(htmlspecialchars($arguments[$i], ENT_QUOTES));
+					}
+				}
+
+				$sql_query = vsprintf(str_replace(self::ARG_CHAR, '%s', $sql_query), $arguments);
+			}
+
+			$GLOBALS['sqlqueries'][] = $sql_query;
+
+			if ($result = $pdo->query($sql_query))
+			{
+				$errored = $result->errorCode();
+
+				// if ($errored)
+				// {
+				//     throw new Exception('Unable to run SQL query. "'. $sql_query. '"<br>'. 'Error: "'. $errored. ': '. $result->errorInfo. '"');
+				// }
+				// else
+				// {
+				return $result;
+				// }
+			}
+			else
+			{
+				throw new Exception('Unable to run SQL query. "'. (!empty($sql_query) ? $sql_query : $orig_query). '"<br>'. (!empty($result->errorInfo) ? ('Error: "'. $result->errorInfo. '"') : 'No error returned!' ));
+				return false;
+			}
+
+		}
+		else
+		{
+			throw new Exception('Unable to connect to Database/PDO Instance.');
+			return false;
+		}
 	}
 } // END class Gallery
