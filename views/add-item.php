f3469b3937cd3f8f75e2c14851a3e934f9990746
Keith McGahey <keef05@gmail.com>
2012-08-28 15:09:21 +1000

    Added add-item view

---
+++ views/add-item.php
@@ -0,0 +1,41 @@
+<?php
+/* Security measure */
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Simple, easy to setup and use gallery plugin for WolfCMS
+ *
+ * @package Plugins
+ * @subpackage Gallery
+ *
+ * @author Keith McGahey
+ */
+?>
+<h1><?php echo __('Add gallery item'); ?></h1>
+
+<p>
+	<?php print_r($item_fields) ?>
+</p>
+
+<form id='addItem'>
+
+</form>
+
+<script type="text/javascript">
+// <![CDATA[
+    function setConfirmUnload(on, msg) {
+        window.onbeforeunload = (on) ? unloadMessage : null;
+        return true;
+    }
+
+    function unloadMessage() {
+        return '<?php echo __('You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.'); ?>';
+    }
+
+    $(document).ready(function() {
+        // Prevent accidentally navigating away
+        $(':input').bind('change', function() { setConfirmUnload(true); });
+        $('form').submit(function() { setConfirmUnload(false); return true; });
+    });
+// ]]>
+</script>
