14af5aa38593e9e293fc18242e3b92f85a8f626b
Keith McGahey <jeffory@c0d.in>
2012-09-11 13:42:40 +1000

    Setup a basic listing view for the items
    Probably needs pagination down the track.

--- views/front-index.php
+++ views/front-index.php
@@ -1,1 +1,26 @@
-<?php
+<h2><?php echo GAL_TITLE ?></h2>
+
+<table class="items_listing">
+<?php foreach ($items as $item): ?>
+	<tr>
+		<td>
+			<div class="thumb_container">
+				<img src="<?php echo URL_PUBLIC. GAL_URL. '/file/image/'. $item['id'] ?>">
+			</div>
+		</td>
+
+		<td>
+			<p>
+				<strong><?php echo $item['code'] ?></strong><br>
+				<?php echo $item['name'] ?>
+			</p>
+		</td>
+
+		<td>
+			<p>
+				<?php echo $item['description'] ?>
+			</p>
+		</td>
+	</tr>
+<?php endforeach; ?>
+</table>
