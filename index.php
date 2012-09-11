a9c9f8211e1856b233cbce6574c38b06696999c1
Keith McGahey <jeffory@c0d.in>
2012-09-11 13:42:01 +1000

    Setup routing for if the plugin is renamed
    Almost all the instances of the plugin can be overidden excluding the
    admin tabs unless you rename the controller class as well.

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 /* Security measure */
 if (!defined('IN_CMS')) { exit(); }
 
-// For printing constants easily in strings
-
 /**
  * Simple, easy to setup and use gallery plugin for WolfCMS
  *
@@ -13,17 +11,18 @@ if (!defined('IN_CMS')) { exit(); }
  * @author Keith McGahey
  */
 
-// 
-define('GAL_ID', 'gallery');
-// Where the folder containing the plugin is...
-define('GAL_ROOT', PLUGINS_ROOT. DS. GAL_ID);
-define('GAL_URL', 'gallery');
-define('GAL_TITLE', 'Gallery');
+// Renaming this allows the Gallery to be used as say a products list or a download listing and so on...
+define('GAL_ID', 'gallery');                        // For the Plugin section in admin
+define('GAL_ROOT', PLUGINS_ROOT. DS. 'gallery');    // Directory root to the plugin
+define('GAL_C_CLASS', 'gallery');                   // Class names, (ie. gallery = GalleryController) Needs to be lowercase!
+
+define('GAL_URL', 'products');                       // Base URL to use the plugin
+define('GAL_TITLE', 'Product Listing');                     // Title of the plugin (for views and what not)
 
 Plugin::setInfos(array(
     'id'                    => GAL_ID,
     'title'                 => __(GAL_TITLE),
-    'description'           => __('Adds a simple and easy to manipulate/use '. __(GAL_TITLE). ' to a website.'),
+    'description'           => __('Adds a simple and easy to manipulate/use '. __(GAL_TITLE). '.'),
     'version'               => '1.0',
    	'license'               => 'Commerical',
 	'author'                => 'Keith McGahey',
@@ -36,10 +35,16 @@ Plugin::setInfos(array(
 AutoLoader::addFile('PluginRecord', GAL_ROOT. DS. 'PluginRecord.php');
 //AutoLoader::addFile('GalleryItem', GAL_ROOT. DS. 'models'. DS. 'GalleryItem.php');
 
-Plugin::addController(GAL_ID, __('Gallery'), 'administrator', true);
-AutoLoader::addFolder(PLUGINS_ROOT. DS. GAL_ID. DS. 'models');      // Sometimes doesn't load?
+Plugin::addController(GAL_C_CLASS, __(GAL_TITLE), 'administrator', true);
+AutoLoader::addFolder(GAL_ROOT. DS. 'models');      // Sometimes doesn't load?
 
+// A lot of the functions have to be re-routed to allow for different plugin names
 Dispatcher::addRoute(array(
-    '/'.GAL_URL. '(|/)'                             => '/plugin/'. GAL_ID. '/front_index',
-    '/'.GAL_URL. '/file/([0-9a-z-]+)/([0-9]+)'      => '/plugin/'. GAL_ID. '/file/$1/$2',
+    '/'. GAL_URL. '(|/)'                                        => '/plugin/'. GAL_C_CLASS. '/front_index',
+    '/admin/plugin/'.GAL_URL. '(|/)'                            => '/plugin/'. GAL_C_CLASS. '/index',
+    '/admin/plugin/'.GAL_URL. '/add'                            => '/plugin/'. GAL_C_CLASS. '/add',
+    '/admin/plugin/'.GAL_URL. '/edit/([0-9]+)'                  => '/plugin/'. GAL_C_CLASS. '/edit/$1',
+    '/admin/plugin/'.GAL_URL. '/delete/([0-9]+)'                => '/plugin/'. GAL_C_CLASS. '/delete/$1',
+    '/admin/plugin/'.GAL_URL. '/clearall'                       => '/plugin/'. GAL_C_CLASS. '/clearall',
+    '/'. GAL_URL. '/file/([0-9a-z-]+)/([0-9]+)(.?(?:[a-z]+)|)'  => '/plugin/'. GAL_C_CLASS. '/file/$1/$2',
 ));
