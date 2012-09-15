0d1e20242a100c7576deab98fd832d79873c0f31
Keith McGahey <jeffory@c0d.in>
2012-09-16 09:21:23 +1000

    Added functionality
    Calls GalleryController::uninstall().

--- uninstall.php
+++ uninstall.php
@@ -11,4 +11,12 @@ if (!defined('IN_CMS')) { exit(); }
  * @author Keith McGahey
  */
 
+error_reporting(E_ALL);
+
+@require_once('GalleryController.php');
+@require_once('models/PluginRecord.php');
+@require_once('models/GalleryItem.php');
+@require_once('models/GalleryItemCat.php');
+@require_once('models/GalleryCat.php');
+GalleryController::uninstall();
 exit();
