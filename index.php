9f01d6f8b5ee868f758725f1b61702fba49bc43c
Keith McGahey <jeffory@c0d.in>
2012-09-18 14:12:45 +1000

    Added a route for categories index

--- index.php
+++ index.php
@@ -41,8 +41,9 @@ AutoLoader::addFolder(GAL_ROOT. DS. 'models');      // Sometimes doesn't load?
 // A lot of the functions have to be re-routed to allow for different plugin names
 Dispatcher::addRoute(array(
     '/'. GAL_URL. '(|/)'                                        => '/plugin/'. GAL_C_CLASS. '/front_category_index',
-
     '/admin/plugin/'.GAL_URL. '(|/)'                            => '/plugin/'. GAL_C_CLASS. '/index',
+    '/admin/plugin/'.GAL_URL. '/categories'                     => '/plugin/'. GAL_C_CLASS. '/category_index',
+    '/admin/plugin/'.GAL_URL. '/categories/page:([0-9]+)'       => '/plugin/'. GAL_C_CLASS. '/category_index/$1',
     '/admin/plugin/'.GAL_URL. '/page:([0-9]+)'                  => '/plugin/'. GAL_C_CLASS. '/index/$1',
     '/admin/plugin/'.GAL_URL. '/add'                            => '/plugin/'. GAL_C_CLASS. '/add',
     '/admin/plugin/'.GAL_URL. '/edit/([0-9]+)'                  => '/plugin/'. GAL_C_CLASS. '/edit/$1',
