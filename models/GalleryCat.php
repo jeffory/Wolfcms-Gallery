927d3c0382b0a5929e31ec489a1fbcf560103032
Keith McGahey <jeffory@c0d.in>
2012-09-18 23:29:46 +1000

    Whitespace fix
    Tabs to spaces.

--- models/GalleryCat.php
+++ models/GalleryCat.php
@@ -12,45 +12,45 @@ if (!defined('IN_CMS')) { exit(); }
  */
 class GalleryCat extends PluginRecord
 {
-	const TABLE_NAME = 'gallery_cat';
-	/**
-	 * Associated database table 
-	 *
-	 * @var string
-	 **/
-	public static $table_name = self::TABLE_NAME;
-	
-	/**
-	 * Database schema, dynamic generates the SQL used for the tables and forms.
-	 * 
-	 * Type (Required):
-	 * integer, string, text, file, datetime
-	 * 
-	 * Options:
-	 *   validation - for validating form fields generated from table
-	 *   allowempty - (true/false) null/not null, also if the field is optional in forms
-	 *   maxlength - (number) table column size, form maxlength
-	 *   userinput - (default: true, true/false) if the field allows user input, ie. if it shows in forms
-	 *   pkey - primary key in table
-	 *   special - currently reserved for the controller setting a value for the model
-	 *   autoinc - Auto increment
-	 *
-	 * TODO: Run this through a function to add default values, eg. strings: maxlength => 255
-	 * Then it can be used in forms and validation.
-	 * 
-	 * @var array
-	 **/
-	public static $table_structure = array(
-		'id' => array(
-			'type' => 'integer',
-			'autoinc' => true,
-			'allowempty' => false,
-			'userinput' => false,
-			'pkey' => true
-			),
-		'category_name' => array(
-			'type' => 'string',
-			'allowempty' => false
-			)
-		);
+    const TABLE_NAME = 'gallery_cat';
+    /**
+     * Associated database table 
+     *
+     * @var string
+     **/
+    public static $table_name = self::TABLE_NAME;
+    
+    /**
+     * Database schema, dynamic generates the SQL used for the tables and forms.
+     * 
+     * Type (Required):
+     * integer, string, text, file, datetime
+     * 
+     * Options:
+     *   validation - for validating form fields generated from table
+     *   allowempty - (true/false) null/not null, also if the field is optional in forms
+     *   maxlength - (number) table column size, form maxlength
+     *   userinput - (default: true, true/false) if the field allows user input, ie. if it shows in forms
+     *   pkey - primary key in table
+     *   special - currently reserved for the controller setting a value for the model
+     *   autoinc - Auto increment
+     *
+     * TODO: Run this through a function to add default values, eg. strings: maxlength => 255
+     * Then it can be used in forms and validation.
+     * 
+     * @var array
+     **/
+    public static $table_structure = array(
+        'id' => array(
+            'type' => 'integer',
+            'autoinc' => true,
+            'allowempty' => false,
+            'userinput' => false,
+            'pkey' => true
+            ),
+        'category_name' => array(
+            'type' => 'string',
+            'allowempty' => false
+            )
+        );
 }
--- models/GalleryItemCat.php
+++ models/GalleryItemCat.php
@@ -12,42 +12,42 @@ if (!defined('IN_CMS')) { exit(); }
  */
 class GalleryItemCat extends PluginRecord
 {
-	const TABLE_NAME = 'gallery_item_cat';
-	/**
-	 * Associated database table 
-	 *
-	 * @var string
-	 **/
-	public static $table_name = self::TABLE_NAME;
-	
-	/**
-	 * Database schema, dynamic generates the SQL used for the tables and forms.
-	 * 
-	 * Type (Required):
-	 * integer, string, text, file, datetime
-	 * 
-	 * Options:
-	 *   validation - for validating form fields generated from table
-	 *   allowempty - (true/false) null/not null, also if the field is optional in forms
-	 *   maxlength - (number) table column size, form maxlength
-	 *   userinput - (default: true, true/false) if the field allows user input, ie. if it shows in forms
-	 *   pkey - primary key in table
-	 *   special - currently reserved for the controller setting a value for the model
-	 *   autoinc - Auto increment
-	 *
-	 * TODO: Run this through a function to add default values, eg. strings: maxlength => 255
-	 * Then it can be used in forms and validation.
-	 * 
-	 * @var array
-	 **/
-	public static $table_structure = array(
-		'item_id' => array(
-			'type' => 'integer',
-			'pkey' => true
-			),
-		'category_id' => array(
-			'type' => 'integer',
-			'pkey' => true
-			)
-		);
+    const TABLE_NAME = 'gallery_item_cat';
+    /**
+     * Associated database table 
+     *
+     * @var string
+     **/
+    public static $table_name = self::TABLE_NAME;
+    
+    /**
+     * Database schema, dynamic generates the SQL used for the tables and forms.
+     * 
+     * Type (Required):
+     * integer, string, text, file, datetime
+     * 
+     * Options:
+     *   validation - for validating form fields generated from table
+     *   allowempty - (true/false) null/not null, also if the field is optional in forms
+     *   maxlength - (number) table column size, form maxlength
+     *   userinput - (default: true, true/false) if the field allows user input, ie. if it shows in forms
+     *   pkey - primary key in table
+     *   special - currently reserved for the controller setting a value for the model
+     *   autoinc - Auto increment
+     *
+     * TODO: Run this through a function to add default values, eg. strings: maxlength => 255
+     * Then it can be used in forms and validation.
+     * 
+     * @var array
+     **/
+    public static $table_structure = array(
+        'item_id' => array(
+            'type' => 'integer',
+            'pkey' => true
+            ),
+        'category_id' => array(
+            'type' => 'integer',
+            'pkey' => true
+            )
+        );
 }
