94db700682ba951f818cd860172343df00a29055
Keith McGahey <jeffory@c0d.in>
2012-09-18 14:14:14 +1000

    Fixed up frontend view
    This is a client thing, will make more generic views when finished.

--- views/front-categories-index.php
+++ views/front-categories-index.php
@@ -1,3 +1,10 @@
-<?php foreach ($categories as $category): ?>
-	<a href="#" class="funky_button"><?php echo $category->category_name ?></a>
-<?php endforeach; ?>
+<div style='width: 550px; text-align: center; margin: 0 auto;'>
+	<h2 style='text-align: left'>Our Products</h2>
+
+	<?php foreach ($categories as $category): ?>
+		<a href="#" class="funky_button"><?php echo $category->category_name ?></a>
+	<?php endforeach; ?>
+	<p>
+		If we don't have a product that you are after please contact us and we will see if we are able to procure it for you.
+	</p>
+</div>
