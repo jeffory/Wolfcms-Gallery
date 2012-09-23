f54821fc4f3060d24cdda1ae299399c1ec7e4513
Keith McGahey <jeffory@c0d.in>
2012-09-23 22:29:40 +1000

    Trimmed down the routes array
    Combined a lot of the pages.

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ define('GAL_ID', 'gallery');                        // For the Plugin section in
 define('GAL_ROOT', PLUGINS_ROOT. DS. 'gallery');    // Directory root to the plugin
 define('GAL_C_CLASS', 'gallery');                   // Class names, (ie. gallery = GalleryController) Needs to be lowercase!
 
-define('GAL_URL', 'gallery');                       // Base URL to use the plugin
-define('GAL_TITLE', 'Gallery');                     // Title of the plugin (for views and what not)
+define('GAL_URL', 'products');                       // Base URL to use the plugin
+define('GAL_TITLE', 'Products');                     // Title of the plugin (for views and what not)
 
 Plugin::setInfos(array(
     'id'                    => GAL_ID,
@@ -40,18 +40,16 @@ AutoLoader::addFolder(GAL_ROOT. DS. 'models');      // Sometimes doesn't load?
 
 // A lot of the functions have to be re-routed to allow for different plugin names
 Dispatcher::addRoute(array(
-    '/'. GAL_URL. '(|/)'                                            => '/plugin/'. GAL_C_CLASS. '/front_category_index',
-    '/admin/plugin/'.GAL_URL. '(|/)'                                => '/plugin/'. GAL_C_CLASS. '/index',
-    '/admin/plugin/'.GAL_URL. '/categories(|/)'                     => '/plugin/'. GAL_C_CLASS. '/category_index',
-    '/admin/plugin/'.GAL_URL. '/categories/page:([0-9]+)'           => '/plugin/'. GAL_C_CLASS. '/category_index/$1',
-    '/admin/plugin/'.GAL_URL. '/categories/delete/([0-9]+)'         => '/plugin/'. GAL_C_CLASS. '/category_delete/$1',
-    '/admin/plugin/'.GAL_URL. '/page:([0-9]+)'                      => '/plugin/'. GAL_C_CLASS. '/index/$1',
-    '/admin/plugin/'.GAL_URL. '/add(|/)'                            => '/plugin/'. GAL_C_CLASS. '/add',
-    '/admin/plugin/'.GAL_URL. '/edit/([0-9]+)'                      => '/plugin/'. GAL_C_CLASS. '/edit/$1',
-    '/admin/plugin/'.GAL_URL. '/delete/([0-9]+)'                    => '/plugin/'. GAL_C_CLASS. '/delete/$1',
-    '/admin/plugin/'.GAL_URL. '/addsamples(|/)'                     => '/plugin/'. GAL_C_CLASS. '/addsamples',
-    '/admin/plugin/'.GAL_URL. '/clearall(|/)'                       => '/plugin/'. GAL_C_CLASS. '/clearall',
-    '/'. GAL_URL. '/file/([0-9a-z-]+)/([0-9]+)(.?(?:[a-z]+)|)'      => '/plugin/'. GAL_C_CLASS. '/file/$1/$2',
+    '/'. GAL_URL. '(|/)'                                                                => '/plugin/'. GAL_C_CLASS. '/front_category_index',
+    '/admin/plugin/'.GAL_URL. '(|/)'                                                    => '/plugin/'. GAL_C_CLASS. '/index',
+
+    '/admin/plugin/'.GAL_URL. '/categories(|/)(?:|)(|page)(|/[0-9]+)'            => '/plugin/'. GAL_C_CLASS. '/category_index$1$2',
+    '/admin/plugin/'.GAL_URL. '/categories/page:([0-9]+)'                               => '/plugin/'. GAL_C_CLASS. '/category_index/$1',
+
+
+    '/admin/plugin/'.GAL_URL. '/categories/delete/([0-9]+)'                             => '/plugin/'. GAL_C_CLASS. '/category_delete/$1',
+    '/admin/plugin/'.GAL_URL. '/(addsamples|clearall|add|edit|delete|page)(|/[0-9]+)'   => '/plugin/'. GAL_C_CLASS. '/$1$2',
+    '/'. GAL_URL. '/file/([0-9a-z-]+)/([0-9]+)(.?(?:[a-z]+)|)'                          => '/plugin/'. GAL_C_CLASS. '/file/$1/$2',
 ));
 
 /**
