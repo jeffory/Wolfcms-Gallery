9abbb22b90b79616f673fdec78e6a4ae24f1422f
Keith McGahey <jeffory@c0d.in>
2012-09-25 00:00:41 +1000

    Started creating category add view

---
+++ views/categories-add.php
@@ -0,0 +1,14 @@
+<?php
+/* Security measure */
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Simple, easy to setup and use gallery plugin for WolfCMS
+ *
+ * @package Plugins
+ * @subpackage Gallery
+ *
+ * @author Keith McGahey
+ */
+?>
+<h1><?php echo __('Add/Edit Category'); ?></h1>
