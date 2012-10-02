d4a0b34c6c38b55286ad8c1e813dd3bb10cc169b
Keith McGahey <jeffory@c0d.in>
2012-10-03 00:33:19 +1000

    Cleaned up front item view
    Looks more complete and professional now.

--- views/front-item.php
+++ views/front-item.php
@@ -8,12 +8,53 @@
  * @author Keith McGahey
  */
 ?>
-<h2><?php echo $item->name ?></h2>
+<h2></h2>
 
-<p>
-	<?php echo $item->description ?>
+<p class='item_image'>
+	<img src='<?php echo URL_PUBLIC. GAL_URL. '/file/image/'. $item->id ?>' style='max-width: 100%'>
 </p>
 
-<p>
-	<?php echo $item->code ?>
+<p class='item_info'>
+	<dl>
+		<dt>Product name</dt>
+		<dd><?php echo $item->name ?></dd>
+
+		<dt>Description</dt>
+		<dd><?php echo $item->description ?></dd>
+
+		<dt>Code</dt>
+		<dd><?php echo $item->code ?></dd>
+
+		<dt>Categories</dt>
+		<dd><?php echo implode(',', $item->category_name) ?></dd>
+	</dl>
 </p>
+
+<p class='item_footer'>
+	<a href="javascript:history.go(-1)">Go back</a>
+</p>
+
+<style type="text/css">
+	.item_image {
+		width: 440px;
+		float: left;
+		margin-right: 40px;
+	}
+	.item_info {
+		margin-top: 70px;
+	}
+	dt, dd {
+		display: inline-block;
+	}
+	dt {
+		width: 180px;
+		float: left;
+	}
+	dt:after {
+		content: ":";
+	}
+	dd {
+		min-width: 120px;
+		clear: right;
+	}
+</style>
