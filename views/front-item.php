88cbea41438d2985247e761a1394d7563c468b34
Keith McGahey <jeffory@c0d.in>
2012-10-02 12:34:53 +1000

    Added unfinished single item view

---
+++ views/front-item.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Simple, easy to setup and use gallery plugin for WolfCMS
+ *
+ * @package Plugins
+ * @subpackage Gallery
+ *
+ * @author Keith McGahey
+ */
+?>
+<h2><?php echo $item->name ?></h2>
+
+<p>
+	<?php echo $item->description ?>
+</p>
+
+<p>
+	<?php echo $item->code ?>
+</p>
