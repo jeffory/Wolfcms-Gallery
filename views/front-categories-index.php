fbe320e0f77611818f36057041ac164391f4a648
Keith McGahey <jeffory@c0d.in>
2015-03-16 13:17:03 +1000

    Added URL_SUFFIX to category links

--- views/front-categories-index.php
+++ views/front-categories-index.php
@@ -10,6 +10,6 @@
 ?>
 <div>
 	<?php foreach ($categories as $category): ?>
-		<a href="<?php echo URL_PUBLIC. GAL_URL. '/'. $category->id. '/'. Node::toSlug($category->category_name) ?>"><?php echo $category->category_name ?></a>
+		<a href="<?php echo URL_PUBLIC. GAL_URL. '/'. $category->id. '/'. Node::toSlug($category->category_name). URL_SUFFIX ?>"><?php echo $category->category_name ?></a>
 	<?php endforeach; ?>
 </div>
